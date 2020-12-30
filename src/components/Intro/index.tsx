6f303f2f03a2ec67390b0e06fa9deeebe08947df
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-12-30 20:55:26 +0100

    Initial commit

---
+++ src/components/Intro/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import './Intro.less';
+
+interface Props {
+
+}
+
+const Intro: FC<Props> = () => {
+  return (
+    <article className="Intro">
+      <h1>Twenty f*cking twenty</h1>    
+    </article>
+  )
+};
+
+export default Intro;
