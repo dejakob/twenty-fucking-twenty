dfa8b64ff608a15b38a54f14ec75b8cc349de478
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2021-01-01 21:25:01 +0100

    Finish up April

--- src/components/April/index.tsx
+++ src/components/April/index.tsx
@@ -6,10 +6,11 @@ import Separator from '../Separator';
 
 import homemadeBurgerImage from '../../assets/homemade-burger.jpg';
 import pastaImage from '../../assets/pasta.jpg';
+import Quote from '../Quote';
 
 export const START_SCROLL_POSITION = END_SCROLL_LAST_ITEM;
 export const END_SCROLL_POSITION = START_SCROLL_POSITION + 400;
-export const CONTENT_SCROLL_DURATION = 800;
+export const CONTENT_SCROLL_DURATION = 400;
 export const END_OF_ALL_SCROLL = END_SCROLL_POSITION + 100 + CONTENT_SCROLL_DURATION + 200;
 export const HIDE_POSITION = END_OF_ALL_SCROLL + 400;
 
@@ -42,6 +43,11 @@ const April = () => {
         imageAlt="Pasta"
         url="https://www.instagram.com/p/B--Kr8Rob07"
       />
+      <Separator />
+      <Quote author="Jakob Kerkhove - While Mcdo is closed">
+        Achievement unlocked: I didn't go to Mcdonalds for like 6 weeks now!
+      </Quote>
+      <Separator />
     </Month>
   );
 };
