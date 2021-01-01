95fdf88c09b086960df9617ea2ea32466554a3a7
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2021-01-02 00:05:34 +0100

    Update deploy script

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 git reset --hard
 git clean -fd
 git checkout master
-npm ci
+npm i
 NODE_ENV=production npm run build
 mkdir docs
-cp -R build/* docs/*
+cp -R build/* docs
 git checkout -b docs
 git add .
 git commit -am 'Build docs'
-git push origin docs
+git push origin docs -f
 git checkout master
-git branch -d docs
+git branch -D docs
 echo "✅ Build successful"
