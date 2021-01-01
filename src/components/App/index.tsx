1acc4498158f6f853344b7950abc514d45097f1d
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2021-01-01 22:14:44 +0100

    Add Tarragona

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import February from '../February';
 import FiredAtLetgo from '../FiredAtLetgo';
 import Intro from '../Intro';
@@ -7,9 +7,15 @@ import March from '../March';
 import April from '../April';
 import './App.scss';
 import May from '../May';
-import June from '../June';
+import June, { HIDE_POSITION } from '../June';
+import useWindowSize from '../../hooks/useWindowSize';
 
 function App() {
+  const { height: windowHeight } = useWindowSize();
+  useEffect(() => {
+    document.body.querySelector('#root')!.setAttribute('style', `height: ${HIDE_POSITION + windowHeight}px`);
+  }, [windowHeight]);
+
   return (
     <main className="App">
       <Intro />
--- src/components/June/index.tsx
+++ src/components/June/index.tsx
@@ -2,6 +2,15 @@ import Month from '../Month';
 import { END_OF_ALL_SCROLL as END_SCROLL_LAST_ITEM } from '../May';
 import Separator from '../Separator';
 import Achievement, { TYPES as ACHIEVEMENT_TYPE } from '../Achievement';
+import InstagramPost from '../InstragamPost';
+import useWindowSize from '../../hooks/useWindowSize';
+import PlaceVisited from '../PlaceVisited';
+import TrainIcon from '../TrainIcon';
+
+import parkGuellImage from '../../assets/park-guell.jpg';
+import plazaEspanaImage from '../../assets/plaza-espana.jpg';
+import moritzImage from '../../assets/moritz.jpg';
+import tarragonaImage from '../../assets/tarragona.jpg';
 
 export const START_SCROLL_POSITION = END_SCROLL_LAST_ITEM;
 export const END_SCROLL_POSITION = START_SCROLL_POSITION + 400;
@@ -10,6 +19,9 @@ export const END_OF_ALL_SCROLL = END_SCROLL_POSITION + 100 + CONTENT_SCROLL_DURA
 export const HIDE_POSITION = END_OF_ALL_SCROLL + 400;
 
 const June = () => {
+  const { width: windowWidth, height: windowHeight } = useWindowSize();
+  const photoSize = Math.min(windowWidth, windowHeight);
+
   return (
     <Month
       contentScrollDuration={CONTENT_SCROLL_DURATION}
@@ -23,6 +35,39 @@ const June = () => {
       <Achievement type={ACHIEVEMENT_TYPE.VIRUS} title="Some restrictions are lifted">
         Not a minute too early... Ough...
       </Achievement>
+      <Separator />
+      <InstagramPost
+        height={photoSize}
+        width={photoSize}
+        imageSrc={parkGuellImage}
+        imageAlt="Park Guëll"
+        url="https://www.instagram.com/p/CBYDV3oIQl-"
+        note="Where are all the tourists???"
+      />
+      <InstagramPost
+        height={photoSize}
+        width={photoSize}
+        imageSrc={plazaEspanaImage}
+        imageAlt="Plaza España"
+        url="https://www.instagram.com/p/CBqLwhko3kk"
+        note="Like, seriously?"
+      />
+      <InstagramPost
+        height={photoSize}
+        width={photoSize}
+        imageSrc={moritzImage}
+        imageAlt="Moritz"
+        url="https://www.instagram.com/p/CBqUjJFonA7"
+      />
+      <Separator />
+      <PlaceVisited
+        placeName="Tarragona"
+        backgroundImage={tarragonaImage}
+        icon={<TrainIcon height={64} width={64} color="#fff" />}
+      >
+        Let's see what's around Barcelona
+      </PlaceVisited>
+      <Separator />
     </Month>
   );
 };
